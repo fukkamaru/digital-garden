de7e3d1daf574076fc6167efee595bdb8fc53c5a
fukkamaru <fukuinfukka@gmail.com>
2026-09-04 03:53:45 +0900

    vault backup: 2026-09-04 03:53:44

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "ふっかまるのデジタルガーデン",
+    pageTitle: "マイ・デジタルガーデン",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
